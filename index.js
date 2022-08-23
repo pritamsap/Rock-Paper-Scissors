a471ec411167bed85b4cd596ba77084f5021555d
PritamSapkota <slicefrost1@gmail.com>
2022-08-23 18:04:41 -0400

    Introduce playRound function with two param
    	The playRound function includes two parameters, playSelection and
    computerSelection. The function has if-else conditions, which compares the
    choice from player and computer (both being lowerCase). After comparing, a string message is declared and stored in a result variable, which displays if the player won or lost and by what choice. Then the result value is returned in the function.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
-/* randomly choose a number between 0-2 and each value is assigned
- rock, paper or scissors */
+// randomly choose a number between 0-2 and each value is assigned
+// rock, paper or scissors
 function getComputerChoice() {
     let Computervalue;
     let randNum = Math.floor(Math.random() * 3);
@@ -18,5 +18,49 @@ function getComputerChoice() {
     return Computervalue;
    
 }
+
+/*Plays a single round a Rock Paper Scissors and return a string delcaring
+why you won or lost - based on your choice of input */
+function playRound(playerSelection, computerSelection) {
+    let result;
+
+    if(playerSelection === computerSelection) {
+        result = "it's a tie!"
+    }
+
+    else if(playerSelection == "rock") {
+        if(computerSelection == "scissors") {
+            result = "You win! Rock beats Scissors"
+        }
+       else if(computerSelection == "paper") {
+            result = "You lose! Paper beats Rock"
+        }
+    }
+
+    else if(playerSelection == "scissors") {
+        if(computerSelection == "rock") {
+            result = "You lose! Rock beats Scissors"
+        }
+       else if(computerSelection == "paper") {
+            result = "You win! Scissors beats Paper"
+        }
+    }
+
+    else if(playerSelection == "paper") {
+        if(computerSelection == "rock") {
+            result = "You win! Paper beats Rock"
+        }
+       else if(computerSelection == "scissors") {
+            result = "You lose! Scissors beats Paper"
+        }
+    }
+
+    return result;
+
+}
+
+
+
+
 
 
