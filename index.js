b8da76f8e1e44506159a944bf8e2135dd76b806d
PritamSapkota <slicefrost1@gmail.com>
2022-08-30 12:24:55 -0400

    Update gameOver and play again button reset function
    	The gameOver function displays a overlaying section, that’s displays game over once the
      round has incremented greater than or equal to 10. The play Again button style position was set to relative so it becomes  visible in the overlay section to reset and play the game again. With resetGame() function, it checks if var isGameOver (true or false) to increment the round or else remove Overlay const attributes.

--- index.js
+++ index.js
@@ -1,56 +1,42 @@
 
-
-// Rough Code
-
-
+// Constant declaration
 const rockChoice = document.querySelector('.rock-div');
 const paperChoice = document.querySelector('.paper-div');
 const scissorsChoice = document.querySelector('.scissors-div');
-
-const scoreCounterPlayer = document.querySelector('.score-update-player')
-const scoreCounterComputer = document.querySelector('.score-update-computer')
-
 const choices = document.querySelectorAll('.rock-div, .paper-div, .scissors-div');
-
 const playAgainBtn = document.querySelector('.play-again');
-
 const headingThree = document.querySelector('.heading-three');
-
 const entirePage = document.querySelector('.entire-page');
 const overlayHeading = document.querySelector('.overlay-heading');
-
-
 const decisionDiv = document.querySelector('.module-extra');
 const resultTxt = document.querySelector('.decision-paragraph');
 const overlay = document.querySelector('.overlay');
+const playerCurrentScore = document.querySelector('.player-score-counter')
+const computerCurrentScore = document.querySelector('.computer-score-counter')
 
-
-let isCondition = true;
-
-rockChoice.addEventListener('click', protocolOne)
-paperChoice.addEventListener('click', protocolTwo)
-scissorsChoice.addEventListener('click', protocolThree)
-playAgainBtn.addEventListener('click', resetter);
+const moduleThree = document.querySelector('.module-three');
 
 
 
-function gameOver() {
-    if(roundIncrementer > 10) {
-        overlay.setAttribute('style', 
-        'width: 100vw; height: 100vh; position: absolute; background-color: #ffb703; position: absolute; transform: scale(0.9); border-radius: 1em;');
-        decisionDiv.style.width = 0;
-        decisionDiv.style.height = 0;
-        resultTxt.textContent = '';
-        overlayHeading.textContent = 'Game Over'
-        playAgainBtn.setAttribute('style', 'position: relative; z-index: 1;');
-        isCondition = false;
-        roundIncrementer = 0;
 
-    }
-    console.log(roundIncrementer);
-    
-}
+// Global Variable
+let isGameOver = false;
+let result = '';
+let playerSum = 0;
+let computerSum = 0;
+let roundIncrementer = 0;
 
+
+
+// All events when clicking on divs/button
+rockChoice.addEventListener('click', getRockResult)
+paperChoice.addEventListener('click', getPaperResult)
+scissorsChoice.addEventListener('click', getScissorsResult)
+playAgainBtn.addEventListener('click', resetGame);
+
+
+
+// applies for all choices
 choices.forEach(choice => {
     choice.addEventListener('click', roundUpdater);
     choice.addEventListener('click', resultOutput);
@@ -60,57 +46,70 @@ choices.forEach(choice => {
 
 
 
+// Display Game Over section
+function gameOver() {
+    if(roundIncrementer >= 10) {
+        overlay.setAttribute('style', 
+        'width: 100vw; height: 100vh; position: absolute; background-color: #ffb703; position: absolute; transform: scale(0.9); border-radius: 1em;');
+        overlayHeading.textContent = 'Game Over'
+        playAgainBtn.setAttribute('style', 'position: relative;');
+        isGameOver = true;
+        roundIncrementer = 0;
+        playerCurrentScore.textContent = 0;
+        computerCurrentScore.textContent = 0;
+        headingThree.textContent = `Choose - [ Round ${roundIncrementer} ] - Weapon`;
 
+    }
+    console.log(roundIncrementer);
+}
+
+
+// outputs result each round
 function resultOutput() {
     decisionDiv.setAttribute('style', 'width: 320px; transform: scale(0.9);');
 }
 
-// if Player one Choosed Rock then, it compares the option with Comp and provides result
 
-let playerSum = 0;
-let computerSum = 0;
-let paragraphPlayer = document.querySelector('.player-score-counter')
-let paragraphComputer = document.querySelector('.computer-score-counter')
-
-let roundIncrementer = 0;
-
-
+// increments round by 1
 function roundUpdater() {
     roundIncrementer += 1;
-    headingThree.textContent = `Choose - [ Round ${roundIncrementer} ] - Weapon`;
-    
-
-    
+    headingThree.textContent = `Choose - [ Round ${roundIncrementer} ] - Weapon`;  
 }
 
-function resetter() {
-    if(isCondition == true) {
-        overlay.setAttribute('style', '');
-        overlayHeading.textContent = '';
+
+
+
+function resetGame() {
+    if(isGameOver == false) {
         roundIncrementer = 1;
     }
-    paragraphComputer.textContent = 0;
-    paragraphPlayer.textContent = 0;
+
+    else {
+        overlay.removeAttribute('style', 'class');
+        overlay.removeChild(overlayHeading);
+    }
+
+    playerCurrentScore.textContent = 0;
+    computerCurrentScore.textContent = 0;
     headingThree.textContent = `Choose - [ Round ${roundIncrementer} ] - Weapon`;
-   
-
-
 }
 
-let result = '';
-function protocolOne() {
+
+
+
+function getRockResult() {
     computerChoice = getComputerChoice();
     console.log(computerChoice);
     if(computerChoice == "scissors") {
         result = "You win! Rock beats Scissors";
         playerSum += 1;
-        paragraphPlayer.textContent = playerSum; 
+        playerCurrentScore.textContent = playerSum; 
     }
 
     else if(computerChoice == "paper"){
         result = "You lose! Paper beats Rock";
         computerSum  += 1;
-        paragraphComputer.textContent = computerSum;
+        computerCurrentScore.textContent = computerSum;
     }
 
     else if(computerChoice == "rock") {
@@ -123,19 +122,19 @@ function protocolOne() {
 
 
 
-function protocolTwo() {
+function getPaperResult() {
     computerChoice = getComputerChoice();
     console.log(computerChoice);
     if(computerChoice == "rock") {
         result = "You win! Paper beats Rock";
         playerSum += 1;
-        paragraphPlayer.textContent = playerSum; 
+        playerCurrentScore.textContent = playerSum; 
     }
 
     else if(computerChoice == "scissors"){
         result = "You lose! Scissors beats Paper";
         computerSum += 1;
-        paragraphComputer.textContent = computerSum;
+        computerCurrentScore.textContent = computerSum;
     }
 
     else if(computerChoice == "paper") {
@@ -143,25 +142,24 @@ function protocolTwo() {
     }
     console.log(result);
     resultTxt.textContent = result;
-
 }
 
 
 
 
-function protocolThree() {
+function getScissorsResult() {
     computerChoice = getComputerChoice();
     console.log(computerChoice);
     if(computerChoice == "paper") {
         result = "You win! Scissors beats Paper";
         playerSum += 1;
-        paragraphPlayer.textContent = playerSum; 
+        playerCurrentScore.textContent = playerSum; 
     }
 
     else if(computerChoice == "rock"){
         result = "You lose! Rock beats Scissors";
         computerSum += 1;
-        paragraphComputer.textContent = computerSum;
+        computerCurrentScore.textContent = computerSum;
     }
 
     else if(computerChoice == "scissors") {
@@ -169,8 +167,6 @@ function protocolThree() {
     }
     console.log(result);
     resultTxt.textContent = result;
-
-
 }
 
 
@@ -192,102 +188,9 @@ function getComputerChoice() {
     else {
         Computervalue = "scissors";
     }
-
-
     return Computervalue;
-   
 }
 
-
-
-
-
-
-// /*Plays a single round a Rock Paper Scissors and return a string delcaring
-// why you won or lost - based on your choice of input */
-// function playRound(playerSelection, computerSelection) {
-//     let result;
-
-//     if(playerSelection === computerSelection) {
-//         result = "it's a tie!"
-//     }
-
-//     else if(playerSelection == "rock") {
-//         if(computerSelection == "scissors") {
-//             result = "You win! Rock beats Scissors"
-//         }
-//        else if(computerSelection == "paper") {
-//             result = "You lose! Paper beats Rock"
-//         }
-//     }
-
-//     else if(playerSelection == "scissors") {
-//         if(computerSelection == "rock") {
-//             result = "You lose! Rock beats Scissors"
-//         }
-//        else if(computerSelection == "paper") {
-//             result = "You win! Scissors beats Paper"
-//         }
-//     }
-
-//     else if(playerSelection == "paper") {
-//         if(computerSelection == "rock") {
-//             result = "You win! Paper beats Rock"
-//         }
-//        else if(computerSelection == "scissors") {
-//             result = "You lose! Scissors beats Paper"
-//         }
-//     }
-
-//     return result;
-
-// }
-
-
-// /* game() function allows the game to be played up to 5 rounds then by comparing
-// the score of player and computer it's declares if the player won or lose out of 5 */
-
-
-// function game() {
-//     let message, resultOutput, playerSelection, computerSelection, messageSplit;
-//     let playerScore = 0; 
-//     let computerScore = 0;
-
-
-//     // for loop - allows 5 rounds of the game
-//     for(let i = 0; i < 5; i++) {
-//         playerSelection = prompt(`Type "rock", "paper", or "scissors" !`).toLowerCase();
-//         computerSelection = getComputerChoice();
-
-
-//         // message stores the value from playRound function, then the string sentence
-//         // is split and put in messageSplit as a array
-
-//         /* This allows index to be compared to see if the player won or lost
-//         and update the score accordinly */
-//         message = playRound(playerSelection, computerSelection);
-//         messageSplit = message.split(" ");
-
-
-//         if(messageSplit.indexOf("win!") == 1) {
-//             playerScore += 1;
-//         }
-//         else if(messageSplit.indexOf("lose!") == 1) {
-//             computerScore += 1;
-//         }       
-//         alert(message);
-//     }
-
-
-//     resultOutput = (playerScore > computerScore) ? "You win" + 
-//     ` ${playerScore} - ${computerScore}` 
-    
-//     : "You lose" + ` ${playerScore} - ${computerScore}` ;
-//     return resultOutput;
-// }
-
-
-// console.log(game());
 
 
 
